f4b08523d52bb2bc58d08f4c6aefad5e235852c4
keith-fernandes <72031382+keith-fernandes@users.noreply.github.com>
2021-08-31 20:34:39 +0100

    master

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// type number 
+
+const myNumber = 3;
+
+const firstNum = 1;
+const secondNum = 2;
+
+const mathOperation = firstNum / secondNum;
+
+console.log(mathOperation)
+
+const floatingPointNum = 3.3;
+const floatingPointNoZero = .5;
+
+const newOperation = firstNum / floatingPointNum;
+
+console.log(newOperation)
+
+// NaN 
+
+const alura = "Alura"; 
+console.log(alura * firstNum)
+
+
---
+++ type-string.js
@@ -0,0 +1,23 @@
+const text1 = "Hello World!";
+const text2 = 'Hello World!';
+const password = "pass1234!"; 
+const stringNum = "32164";
+
+const cit = "My name is ";
+const myName = "Keith"; 
+
+//concat concatenation
+
+console.log(cit + myName)
+
+const right = '\u2705';
+console.log(right)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
