e92cd2a9c119d3b2367681168d7d7bcc07056343
keith-fernandes <72031382+keith-fernandes@users.noreply.github.com>
2021-09-08 09:22:56 +0100

    mycommit

--- conversions.js
+++ conversions.js
@@ -8,8 +8,22 @@ const stringNum= "456";
 
 console.log(num == stringNum)
 console.log(num + stringNum)
-
-//Number()
-console.log(num + Number(stringNum)) //conversion of a string to a number 
+ 
 
 //explicit conversion
+
+//Number()
+console.log(num + Number(stringNum)) //conversion of a string to a number
+
+// Vamos calcular a área de um retângulo
+let largura = "10";
+let altura = "5";
+console.log( Number(largura) * Number(altura)); // teremos a conversão de String para números, possibilitando a realização da da multiplicação
+
+//using + to convert
+console.log( + largura * + altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
+
+let usuarioConectado= false;
+console.log (Number( usuarioConectado ) ); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+usuarioConectado= true;
+console.log (Number( usuarioConectado ) ); // agora teremos a conversão de true (verdadeiro) para o número 1.
