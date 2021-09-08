6dfddfac1c07e5e34ddb35d781d7a04404a54e8a
keith-fernandes <72031382+keith-fernandes@users.noreply.github.com>
2021-09-08 10:00:37 +0100

    mycommit

---
+++ console.js
@@ -0,0 +1,11 @@
+// log -> register
+// const myVar = true;
+
+// console.log(245)
+// console.log("hello world")
+// console.log(myVar)
+
+// error processing
+
+console.error(new Error('that is an error'))
+console.log('that is an error')
--- script.js
+++ script.js
@@ -25,15 +25,17 @@
 
 //const
 
-const shape = 'triangle';
-const height= 5;
-const length= 6;
-let area;
+// const shape = 'triangle';
+// const height= 5;
+// const length= 6;
+// let area;
 
-if(shape ==='square'){
-    area = height*length;
-}else{
-    area = (height*length)/2;
-}
+// if(shape ==='square'){
+//     area = height*length;
+// }else{
+//     area = (height*length)/2;
+// }
 
-console.log(area)
+// console.log(area)
+
+//dinamic type
